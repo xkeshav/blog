7b44e2ad6f22cdb2561e232c5a6f4c51ca42b6b4
Keshav Mohta <xkeshav@gmail.com>
2023-12-27 23:40:31 +0530

    added uno css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xkeshav.netlify.app/',
-  integrations: [mdx(), sitemap(), UnoCSS({ injectReset: true })],
+  integrations: [mdx(), sitemap(), UnoCSS({ injectReset: false })],
 });
